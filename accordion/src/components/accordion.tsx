e1005920b0694434923feebed6166e6e3e67f570
Faisal Alabdullateef <101182040+Falabdullateef@users.noreply.github.com>
2025-05-10 21:55:11 +0300

    Update accordion.tsx
    Added the core functionality of the accordion single selection.

--- accordion/src/components/accordion.tsx
+++ accordion/src/components/accordion.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 const Accordion = () => {
+  const [selected, setSelected] = useState<string | null>(null);
+
+  function handlePlusClick(id: string) {
+    console.log("You have clicked question: " + id);
+    if (selected == id) {
+      setSelected(null);
+      console.log("Set Selected is: " + selected);
+      return;
+    }
+    setSelected(id);
+    console.log("Set Selected is: " + selected);
+  }
+
   return (
-    <div>
-      <div>
+    <div className="wrapper">
+      <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
               <div className="title">
                 <h3>{dataItem.question}</h3>
-                <span>+</span>
+                <span
+                  onClick={() => {
+                    handlePlusClick(dataItem.id);
+                  }}
+                >
+                  +
+                </span>
+                {selected === dataItem.id ? (
+                  <div className="answer">{dataItem.answer}</div>
+                ) : null}
               </div>
             </div>
           ))
