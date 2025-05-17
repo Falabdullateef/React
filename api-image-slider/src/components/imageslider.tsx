ff0fcdea4257344623064223aa15b02219dc680d
Faisal Alabdullateef <101182040+Falabdullateef@users.noreply.github.com>
2025-05-17 19:40:18 +0300

    Full functionalization of the imageslider
    This commit fully functionalizes the image slider component from image navigation to error handling. Further styling improvement is required to make the image fully visible to the user and the other components.

--- api-image-slider/src/components/imageslider.tsx
+++ api-image-slider/src/components/imageslider.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BsArrowUpCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
-const imageslider = () => {
-  return <div>imageslider</div>;
+interface Props {
+  url: string;
+  limit: string;
+}
+
+const imageslider = ({ url, limit }: Props) => {
+  const [images, setImages] = useState<any[]>([]);
+  const [currentslide, setCurrentSlide] = useState(0);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchImages(geturl: string) {
+    try {
+      const response = await fetch(`${geturl}?page=1&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e: unknown) {
+      setLoading(false);
+      if (e instanceof Error) {
+        setErrorMessage(e.message);
+      } else {
+        setErrorMessage("an unknown error occurred");
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]); // Add dependency array
+  console.log(images);
+
+  if (loading) {
+    return <div>Loading Data, Please Wait</div>;
+  }
+  if (errorMessage !== null) {
+    return <div>Ran into an error. Error message: {errorMessage}</div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowRightCircleFill className="arrow arrow-left" />
+      {images && images.length
+        ? images.map((imageItem) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className="current-image"
+            ></img>
+          ))
+        : null}
+      <BsArrowRightCircleFill className="arrow arrow-right"></BsArrowRightCircleFill>
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button key={index} className="current-indicator"></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
 };
 
 export default imageslider;
