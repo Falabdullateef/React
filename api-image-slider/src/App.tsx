fadf9a62c6a15d6513c1ce69cce78188dd371dc6
Faisal Alabdullateef <101182040+Falabdullateef@users.noreply.github.com>
2025-05-17 18:50:41 +0300

    Initialized new image slider component using vite
    Check out readme.md file for more info on the description of this entire project.

---
+++ api-image-slider/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ImageSlider from "./components/imageslider";
+
+const App = () => {
+  return <ImageSlider />;
+};
+
+export default App;
---
+++ api-image-slider/src/components/imageslider.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const imageslider = () => {
+  return <div>imageslider</div>;
+};
+
+export default imageslider;
