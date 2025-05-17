decd0ef76982043ec9c96fbaf03976f5bcb62a4e
Faisal Alabdullateef <101182040+Falabdullateef@users.noreply.github.com>
2025-05-17 19:38:38 +0300

    Added dynamic parameters for the imageslider component
    This allows users to input their custom url (API point) and limit . Limit is how much data should be fetched from the api.

--- api-image-slider/src/App.tsx
+++ api-image-slider/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ImageSlider from "./components/imageslider";
 
 const App = () => {
-  return <ImageSlider />;
+  return <ImageSlider url="https://picsum.photos/v2/list" limit={"10"} />;
 };
 
 export default App;
