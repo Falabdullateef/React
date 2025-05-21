33b23dc5d99fa1e90260e6a3dc541c66f9f76040
Faisal Alabdullateef <101182040+Falabdullateef@users.noreply.github.com>
2025-05-21 21:22:03 +0300

    Added main fetch functionality for the component.
    The function uses a async method and catches any errors in the process.

--- load-more-api/src/components/load-more.tsx
+++ load-more-api/src/components/load-more.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const LoadMore = () => {
+  const [loading, setloading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+
+  async function fetchProducts() {
+    try {
+      const reponse = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const result = await reponse.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return <div>LoadMore</div>;
 };
 
